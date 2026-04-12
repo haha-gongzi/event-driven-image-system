a64663650cbdf3c64a0f3d6837fc5527097f63c5
haha-gongzi <15906572525@163.com>
2026-04-12 18:04:03 -0400

    broker_interface.py
    Added docstrings to publish and subscribe methods.

--- app/broker/broker_interface.py
+++ app/broker/broker_interface.py
@@ -3,7 +3,9 @@ from typing import Callable, Protocol
 
 class BrokerInterface(Protocol):
     def publish(self, topic: str, event: dict) -> None:
+        """Publish an event to a topic"""
         ...
 
     def subscribe(self, topic: str, handler: Callable[[dict], None]) -> None:
+        """Subscribe to a topic with a handler"""
         ...
