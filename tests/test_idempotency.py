a7720eb48b4d157ae0a0bf409e76916c5d12133d
haha-gongzi <15906572525@163.com>
2026-04-21 13:57:08 -0400

    Rename test for unique event IDs and update topic
    test_idempotency.py

--- tests/test_idempotency.py
+++ tests/test_idempotency.py
@@ -1,7 +1,8 @@
-def test_event_ids_are_unique():
-    from app.events.schemas import make_event
+from app.events.schemas import make_event
 
-    e1 = make_event("test.topic", {"id": 1})
-    e2 = make_event("test.topic", {"id": 1})
+
+def test_generated_event_ids_are_unique():
+    e1 = make_event("image.submitted", {"image_id": "img_001"})
+    e2 = make_event("image.submitted", {"image_id": "img_001"})
 
     assert e1.event_id != e2.event_id
